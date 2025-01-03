0496757d07210307abc3a1012e3f4158031ddfe1
jkkj <rhythmgulati11@gmail.com>
2025-01-04 00:08:09 +0530

    day2

---
+++ Functions.java
@@ -0,0 +1,76 @@
+import java.util.Scanner;
+
+// public class Functions {
+
+// public static int average(int a, int b, int c) {
+// int average = (a + b + c) / 3;
+// return average;
+// }
+
+// public static void main(String[] args) {
+// Scanner sc = new Scanner(System.in);
+// int a = sc.nextInt();
+// int b = sc.nextInt();
+// int c = sc.nextInt();
+// int average = average(a, b, c);
+// System.out.println("average is" + average);
+// }
+// }
+
+// public class Functions {
+
+// public static int sum(int a) {
+// int sum = 0;
+// for (int i = 0; i <= a; i++) {
+// if (i % 2 != 0) {
+// sum += i;
+// System.out.println(i);
+// }
+// }
+// return sum;
+// }
+
+// public static void main(String[] args) {
+// Scanner sc = new Scanner(System.in);
+// int a = sc.nextInt();
+// int sum = sum(a);
+// System.out.println("sum is: " + sum);
+// }
+// }
+
+// public class Functions {
+
+// public static void main(String[] args) {
+// do {
+// System.out.println("a");
+// } while (true);
+// }
+// }
+
+// public class Functions {
+
+// public static int fibonacci(int a) {
+
+// int one = 0, two = 1;
+// if (a <= 0) {
+// System.out.println(a);
+// } else if (a == 1) {
+// System.out.println(one + "" + two);
+// } else {
+// for (int i = 1; i <= a; i++) {
+// System.out.println(two);
+// int temp = two;
+// two = one + two;
+// one = temp;
+
+// }
+// }
+// return 0;
+// }
+
+// public static void main(String[] args) {
+// Scanner sc = new Scanner(System.in);
+// int a = sc.nextInt();
+// fibonacci(a);
+// }
+// }
