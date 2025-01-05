851b26967558643c304851625429f2b94526868b
jkkj <rhythmgulati11@gmail.com>
2025-01-05 14:53:02 +0530

    day4

--- Strings.java
+++ Strings.java
@@ -115,3 +115,41 @@
 
 // }
 // }
+
+// import java.util.Scanner;
+
+// public class Strings {
+// public static void main(String[] args) {
+// Scanner sc = new Scanner(System.in);
+// String str = sc.next();
+// String result = "";
+// for (int i = 0; i < str.length(); i++) {
+// if (str.charAt(i) == 'e') {
+// result += "i";
+// } else {
+// result += str.charAt(i);
+// }
+// }
+// System.out.println(result);
+
+// }
+// }
+
+// import java.util.Scanner;
+
+// public class Strings {
+// public static void main(String[] args) {
+// Scanner sc = new Scanner(System.in);
+// String str = sc.next();
+// String result = "";
+// for (int i = 0; i < str.length(); i++) {
+// if (str.charAt(i) == '@') {
+// break;
+// } else {
+// result += str.charAt(i);
+// }
+// }
+// System.out.println(result);
+
+// }
+// }
